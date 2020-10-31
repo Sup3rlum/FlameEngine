468ab583bab93793fb7b44d89ec2066d60d27f38
Toma Kolev <S0207933@fecolleges.london>
2020-10-31 21:27:28 +0000

    .

--- AssetEncoderCommandline/Compilers/ModelFileCompiler.cs
+++ AssetEncoderCommandline/Compilers/ModelFileCompiler.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Collections.Generic;
 using System.Text;
+using AssetEncoderCommandline.Math;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
 
 namespace AssetEncoderCommandline.Compilers
 {
